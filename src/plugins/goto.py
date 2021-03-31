ea8f74bbe2d0964a65fbaa788230383624ec5fb9
Hopyoprop <yeo_jordan@yahoo.com.sg>
2021-03-31 22:15:58 +0800

    Updated GOTO Plugin
    - Completed

--- src/plugins/goto.py
+++ src/plugins/goto.py
@@ -1,6 +1,7 @@
 import logging
 import utils
 from obsfucation import Obfuscation
+import re
 
 logger = logging.getLogger(
     "{0}".format(__name__)
@@ -14,17 +15,30 @@ def obfuscate(obsfucation: Obfuscation):
             with utils.edit_file(smali_file) as (in_file, out_file):
                 control = False                        # Control for when to Insert respective Obfuscation.
                 for line in in_file:
+                    # Only add a GOTO if the Method is not abstract or native
                     if (
                             line.startswith(".method ") and " abstract " not in line
                             and " native " not in line and not control
                     ):
                         out_file.write(line)
                         control = True
-                        print(line)
 
+                    # Checks for a method and validates if the control is True
+                    elif control and re.search(r"\s+\.locals\s(?P<local_count>\d+)", line):
+                        out_file.write(line)
+                        out_file.write('\n\tgoto/32 :GotoEND\n')        # Jump to 'GotoEND' Label
+                        out_file.write('\n\t:GotoStart\n')              # Add a Label for Jumping
+
+                    # Checks for the end of a method and validates if the control is True
+                    elif control and re.search(r'^([ ]*?)\.end method', line):
+                        out_file.write("\n\t:GotoEND\n")                # Add a Label for Jumping called GotoEND
+                        out_file.write("\n\tgoto/32 :GotoStart\n")      # Jump to 'GotoStart' Label
+                        out_file.write(line)
+                        control = False                                 # Turn off control to prevent any modification
+
+                    # Write out line as per normal if no match
                     else:
                         out_file.write(line)
-
 
     except Exception as e:
         logger.error(
@@ -34,4 +48,4 @@ def obfuscate(obsfucation: Obfuscation):
         )
         raise
 
-    # print(obsfucation.smali_files)
+
